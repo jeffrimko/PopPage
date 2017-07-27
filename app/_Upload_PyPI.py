e6246e584078aaf458f06e6ed8f004585d174752
Jeff Rimko <jeffrimko@gmail.com>
2017-07-26 22:04:41 -0400

    Updates for new release.

---
+++ app/_Upload_PyPI.py
@@ -0,0 +1,36 @@
+"""Uploads the package to PyPI."""
+
+##==============================================================#
+## SECTION: Imports                                             #
+##==============================================================#
+
+import auxly.shell as sh
+import sys
+
+import qprompt
+
+sys.path.append("..")
+sys.dont_write_bytecode = True
+
+from _Check_Versions import VERCHK
+from _Install_Package import generate_readme, cleanup_readme
+
+##==============================================================#
+## SECTION: Main Body                                           #
+##==============================================================#
+
+if __name__ == '__main__':
+    pause = True
+    if len(sys.argv) > 1 and "nopause" == sys.argv[1]:
+        pause = False
+    ver = VERCHK.run()
+    if not ver:
+        qprompt.alert("Issue with version info!")
+        sys.exit(1)
+    if qprompt.ask_yesno("Upload version `%s`?" % (ver)):
+        generate_readme()
+        sh.call("python setup.py sdist upload")
+        cleanup_readme()
+    if pause:
+        qprompt.pause()
+    sys.exit(0)
