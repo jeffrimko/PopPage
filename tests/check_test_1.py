06870195a718c1c1759280f7370de8963cad5022
Jeff Rimko <jeffrimko@gmail.com>
2017-09-05 21:11:32 -0400

    Updated test to include GitHub and fixed typo.

--- tests/check_test_1.py
+++ tests/check_test_1.py
@@ -16,30 +16,40 @@ from poppage import check
 class TestCase(BaseTest):
 
     def test_check_1(test):
-        """Check for basic check functionality."""
+        """Basic check functionality."""
         tvars = check("./templates/t1.jinja2")
         test.assertEqual(sorted(tvars), ["name"])
 
     def test_check_2(test):
-        """Check for basic check functionality."""
+        """Basic check functionality."""
         tvars = check("./templates/t2.jinja2")
         test.assertEqual(sorted(tvars), ["name::first", "name::last"])
 
     def test_check_3(test):
-        """Check for basic check functionality."""
+        """Basic check functionality."""
         tvars = check("./templates/t3.jinja2")
         test.assertEqual(sorted(tvars), ["name::first", "name::last", "num"])
 
     def test_check_4(test):
-        """Check for basic check functionality."""
+        """Basic check functionality."""
         tvars = check("./templates/t4.jinja2")
         test.assertEqual(sorted(tvars), ["items"])
 
-    def test_check_4(test):
-        """Check for basic check functionality."""
+    def test_check_5(test):
+        """Basic check functionality."""
         poppage.KEYSEP = "->"
         tvars = check("./templates/t3.jinja2")
         test.assertEqual(sorted(tvars), ["name->first", "name->last", "num"])
+
+    def test_check_6(test):
+        """GitHub check functionality."""
+        poppage.KEYSEP = "::"
+        tvars = check("https://github.com/solarnz/cookiecutter-avr")
+        evars = []
+        evars.append("cookiecutter::full_name")
+        evars.append("cookiecutter::repo_name")
+        evars.append("cookiecutter::year")
+        test.assertEqual(sorted(tvars), sorted(evars))
 
 ##==============================================================#
 ## SECTION: Main Body                                           #
