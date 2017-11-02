5fb7e601c579ec44699b098eaa73df42357cfe9c
Jeff Rimko <jeffrimko@gmail.com>
2017-11-01 23:17:16 -0400

    Bumped version number.

--- app/setup.py
+++ app/setup.py
@@ -22,7 +22,7 @@ if "Linux" == system():
 
 setup(
     name = "poppage",
-    version = "0.6.2",
+    version = "0.6.3",
     author = "Jeff Rimko",
     author_email = "jeffrimko@gmail.com",
     description = "Utility for generating files and directories from templates.",
