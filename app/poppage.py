637a10a51c683f81188ba3af98182fd7092dcfaf
Jeff Rimko <jeffrimko@gmail.com>
2018-02-22 20:28:03 -0500

    Bumped version.

--- app/poppage.py
+++ app/poppage.py
@@ -86,7 +86,7 @@ if sys.version_info < (3, 0):
 ##==============================================================#
 
 #: Application version string.
-__version__ = "0.6.8"
+__version__ = "0.7.0"
 
 #: Key separator.
 KEYSEP = "::"
